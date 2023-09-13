335c16c3ce8a7c4346f5301b3fc3c7b15b9a4c5f
ClevelandIII <kingslime108@gmail.com>
2023-09-12 20:05:52 -0700

    Basically done with 6, need map and gamemode rando + other games

--- bloonsRNG.js
+++ bloonsRNG.js
@@ -1,4 +1,4 @@
-`use strict`
+`use strict`;
 
 //CSS Color Vars
 let green = "rgb(84, 222, 0, 1)";
@@ -6,18 +6,18 @@ let darkGreen = "rgb(60, 181, 0, 1)";
 let brown = "rgb(176, 123, 68, 1)";
 let darkBrown = "rgb(159, 120, 67, 1)";
 
-var css = document.querySelector(':root');
+var css = document.querySelector(":root");
 
 function myFunction_set() {
-    css.style.setProperty('--green', green);
-    css.style.setProperty('--dark-green', darkGreen);
-    css.style.setProperty('--brown', brown);
-    css.style.setProperty('--dark-brown', darkBrown);
+    css.style.setProperty("--green", green);
+    css.style.setProperty("--dark-green", darkGreen);
+    css.style.setProperty("--brown", brown);
+    css.style.setProperty("--dark-brown", darkBrown);
 }
 
 //Dropdown Functionality
 function dropdown(id) {
-    let change = document.getElementById(id + "Contained")
+    let change = document.getElementById(id + "Contained");
 
     if (change.style.display === "block") {
         change.style.display = "none";
@@ -28,89 +28,152 @@ function dropdown(id) {
 
 //Button Onclick
 let heroArray = [];
-let towerArray = []
-let numRandom = 0
+let towerArray = [];
+let numRandom = 0;
 
 function takeHero(id) {
-    let change = document.getElementById(id)
+    let change = document.getElementById(id);
 
-    if (change.style.backgroundColor === "rgb(176, 123, 68, 1)" || change.style.backgroundColor === "rgb(176, 123, 68)") {
+    if (
+        change.style.backgroundColor === "rgb(176, 123, 68, 1)" ||
+        change.style.backgroundColor === "rgb(176, 123, 68)"
+    ) {
         change.style.backgroundColor = "rgb(176, 123, 68, 0.5)";
-        change.style.borderColor = "rgb(159, 120, 67, 0.5)"
+        change.style.borderColor = "rgb(159, 120, 67, 0.5)";
 
-        //thx stack 
+        //thx stack
         const index = heroArray.indexOf(id);
         if (index > -1) {
             heroArray.splice(index, 1);
         }
-        console.log(heroArray)
+        console.log(heroArray);
     } else {
         change.style.backgroundColor = "rgb(176, 123, 68, 1)";
-        change.style.borderColor = "rgb(159, 120, 67, 1)"
+        change.style.borderColor = "rgb(159, 120, 67, 1)";
 
-        heroArray.push(id)
-        console.log(heroArray)
+        heroArray.push(id);
+        console.log(heroArray);
     }
 }
 
 function takeTower(id) {
-    let change = document.getElementById(id)
+    let change = document.getElementById(id);
 
-    if (change.style.backgroundColor === "rgb(176, 123, 68, 1)" || change.style.backgroundColor === "rgb(176, 123, 68)") {
+    if (
+        change.style.backgroundColor === "rgb(176, 123, 68, 1)" ||
+        change.style.backgroundColor === "rgb(176, 123, 68)"
+    ) {
         change.style.backgroundColor = "rgb(176, 123, 68, 0.5)";
-        change.style.borderColor = "rgb(159, 120, 67, 0.5)"
+        change.style.borderColor = "rgb(159, 120, 67, 0.5)";
 
-        //thx stack 
+        //thx stack
         const index = towerArray.indexOf(id);
         if (index > -1) {
             towerArray.splice(index, 1);
         }
-        console.log(towerArray)
+        console.log(towerArray);
     } else {
         change.style.backgroundColor = "rgb(176, 123, 68, 1)";
-        change.style.borderColor = "rgb(159, 120, 67, 1)"
+        change.style.borderColor = "rgb(159, 120, 67, 1)";
 
-        towerArray.push(id)
-        console.log(towerArray)
+        towerArray.push(id);
+        console.log(towerArray);
     }
 }
 
 function num(value) {
-    numRandom = value
+    numRandom = value;
+    console.log(numRandom);
 }
 
 //Submit
-let finalBox = document.getElementById("endBox")
-let finalText = document.getElementById("endDialogue")
+let finalBox = document.getElementById("endBox");
+let finalText = document.getElementById("endDialogue");
 let numTowers;
 
 function submitData() {
+    //Sets numTowers with one hero
     if (heroArray.length >= 1) {
         numTowers = towerArray.length + 1;
     } else {
-        numTowers = towerArray.length
+        numTowers = towerArray.length;
     }
 
-    console.log(numTowers)
+    finalBox.style.display = "block";
 
-    if (numRandom != 0 && numRandom != "" && numRandom != " ") {
-        finalBox.style.display = "block"
+    //Final display text
+    if (numRandom == 0 || towerArray.length + heroArray.length == 0) {
+        //No towers to rando
+        finalText.textContent = `You have selected no towers.`;
+    } else if (numRandom == 1 || towerArray.length == 0 || (heroArray.length == 0 && towerArray.length == 1)) {
+        //Only one tower to rando. Defaults to hero if hero otherwise one tower
+        if (heroArray.length > 0) {
+            let chosenHero;
+            let heroRNG;
 
-        if (numRandom < numTowers) {
-            if(heroArray.length > 0){
-                heroRNG = Math.floor(Math.random() )
+            heroRNG = Math.floor(Math.random() * heroArray.length);
+            chosenHero = heroArray[heroRNG];
+
+            finalText.textContent = `You selected ${
+                heroArray.length + towerArray.length
+            } heros and 1 hero to be chosen, and your hero is ${chosenHero}.`;
+        } else {
+            for (i = 0; i < numRandom; i++) {
+                let towerRNG;
+                let chosenTowers;
+
+                towerRNG = Math.floor(Math.random() * towerArray.length);
+                chosenTowers = towerArray[towerRNG];
+
+                finalText.textContent = `You selected ${
+                    heroArray.length + towerArray.length
+                } towers, and your tower is ${chosenTowers}.`;
             }
-            for (i = 0; i > 1; i++) {
-                let RNG = Math.floor(Math.random() * numRandom)
-            }
-
-            finalText.textContent = `You selected ${heroArray.length + towerArray.length} towers and your ${numTowers} towers are ${Math}`
         }
     } else {
-        finalBox.style.display = "block"
+        //Several towers to rando
+        if (heroArray.length > 0) {
+            let chosenHero;
+            let heroRNG;
 
-        if (numRandom > numTowers) {
-            finalText.textContent = "Yeah nah"
+            heroRNG = Math.floor(Math.random() * heroArray.length);
+            chosenHero = heroArray[heroRNG];
+
+            finalText.textContent = `You selected ${
+                heroArray.length + towerArray.length
+            } towers and ${numTowers} overall towers. Your hero is ${chosenHero} and your towers are `;
+
+            for (i = 0; i < numRandom - 1; i++) {
+                let towerRNG;
+                let chosenTowers;
+
+                towerRNG = Math.floor(Math.random() * towerArray.length);
+                chosenTowers = towerArray[towerRNG];
+
+                if (i + 1 != (numRandom - 1)) {
+                    finalText.textContent += `${chosenTowers}, `;
+                } else {
+                    finalText.textContent += `and ${chosenTowers}.`;
+                }
+            }
+        } else {
+            finalText.textContent = `You selected no hero, ${
+                heroArray.length + towerArray.length
+            } towers and ${numTowers} overall towers. Your towers are `;
+
+            for (i = 0; i < numRandom; i++) {
+                let towerRNG;
+                let chosenTowers;
+
+                towerRNG = Math.floor(Math.random() * towerArray.length);
+                chosenTowers = towerArray[towerRNG];
+
+                if (i + 1 != numRandom) {
+                    finalText.textContent += `${chosenTowers}, `;
+                } else {
+                    finalText.textContent += `and ${chosenTowers}.`;
+                }
+            }
         }
     }
 }
